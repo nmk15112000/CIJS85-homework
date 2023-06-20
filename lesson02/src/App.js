0f2011dd7baf25fb2cc5e71599a558b202c5d317
nmk15112000 <mauquipro1@gmail.com>
2023-06-20 21:53:04 +0700

    Push 1

---
+++ lesson02/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <input type="text" placeholder="Enter your task here" classname="input"></input>
+      <div classname="element">
+        <input type="radio" />
+        <label class="text">Clean up bedroom</label>
+      </div>
+      <div classname="element">
+        <input type="radio" />
+        <label class="text">Buy some milk</label>
+      </div>
+      <div classname="element">
+        <input type="radio" />
+        <label class="text">Jogging</label>
+      </div>
+      <div classname="element">
+        <input type="radio" />
+        <label class="text">Learn React</label>
+      </div>
+      <div classname="element">
+        <input type="radio" />
+        <label class="text">Doing exercises</label>
+      </div>
+      <footer>
+        <p class="right">5 task left!</p>
+        <p class="left">MindX todolist</p>
+      </footer>
+    </div>
+    
+  )
+};
+export default App; 
