0f4665fdd5955e4361d9b7586fc67ad95893ac38
nmk15112000 <mauquipro1@gmail.com>
2023-06-23 00:35:01 +0700

    Push 4

---
+++ lesson03/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <div class="image_bar">
+        <h1>Shop App</h1>
+        <img class="image" src="lesson03/src/image.png" alt=""></img>
+      </div>
+      <div class="Register_bar">
+        <h1>Welcome to Shop App</h1>
+      <input type="text" placeholder="UserName" classname="input"></input><hr/>
+      <input type="text" placeholder="Email@mail.com" classname="input"></input><hr/>
+      <input type="text" placeholder="Password" classname="input"></input><hr/>
+      <input type="text" placeholder="Confirm Password" classname="input"></input><hr/>
+      <h2 class="Button">Register</h2>
+      <p>Login</p>
+    </div>
+    </div>
+    
+  )
+};
+export default App; 
